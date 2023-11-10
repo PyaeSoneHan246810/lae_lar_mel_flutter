de2a2c689f8068cf8bd531375aadb8fbe1398b49
Pyae Sone Han <pyaesonehan246810@gmail.com>
2023-11-10 19:11:49 +0630

    10/11/2023

--- lib/app/pages/notifications_page.dart
+++ lib/app/pages/notifications_page.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:lae_lar_mel_app/app/widgets/custom_appbar_with_back_arrow_and_title.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+import '../config/colors.dart';
+import '../config/font_styles.dart';
+
 class NotificationsPage extends StatefulWidget {
   const NotificationsPage({super.key});
 
@@ -12,12 +15,43 @@ class NotificationsPage extends StatefulWidget {
 class _NotificationsPageState extends State<NotificationsPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomAppBarWithBackArrowAndTitle(
-        titleText: AppLocalizations.of(context)!.notifications,
-        onBackButtonPressed: () {
-          Navigator.pop(context);
-        },
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: CustomAppBarWithBackArrowAndTitle(
+          titleText: AppLocalizations.of(context)!.notifications,
+          onBackButtonPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        body: Column(
+          children: [
+            const TabBar(
+              tabs: [
+                Tab(text: 'Featured'),
+                Tab(text: 'Course Info'),
+                Tab(text: 'Promotions'),
+                Tab(text: 'Software Updates'),
+              ],
+              isScrollable: true,
+              indicatorColor: AppColor.primaryColor,
+              labelStyle: AppFontStyle.tabNavText,
+              labelPadding: EdgeInsets.only(left: 18, right: 18),
+            ),
+            Expanded(
+              child: TabBarView(
+                children: [
+                  Center(
+                    child: Text(
+                      'No notifications yet.',
+                      style: AppFontStyle.bodyOffBlack(context),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
